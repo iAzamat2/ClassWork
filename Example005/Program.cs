5b2ff7daf8712a780adfb70f089fbd9fd33a349e
Azamat <isabirovazamati@gmail.com>
2022-07-09 19:30:03 +0500

    Add Example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести все числа из отрезка [-N, N]
+void Zadacha7()
+{
+    Console.WriteLine("Введите число N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    N = Math.Abs(N);
+    //int a = -N; int b = N;
+    for (int i = -N; i <= N; i++)
+    {
+        Console.Write(i + " ");
+
+    }
+    Console.WriteLine();
+}
+
+Zadacha7();
