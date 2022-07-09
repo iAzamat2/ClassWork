54676597ea1a9eb3cac4295ddd24b7ff193405ab
Azamat <isabirovazamati@gmail.com>
2022-07-09 19:27:11 +0500

    Add Example 001

---
+++ Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа
+int result;
+Console.WriteLine("Введите число, квадрат которого нужно вычислить");
+int number = Convert.ToInt32(Console.ReadLine());
+result = number * number;
+Console.WriteLine($"{number}^2 = {result}");
+//Console.WriteLine("Квадрат числа равен "+result);
