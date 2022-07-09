f6658f3d4e5d3e0b5e29b57cb018754697590294
Azamat <isabirovazamati@gmail.com>
2022-07-09 19:28:09 +0500

    Add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,7 @@
+﻿// Проверка равно ли первое число квадрату второго числа
+Console.WriteLine("Введите первое число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = b * b;
+Console.WriteLine((a == res) ? "Да" : "Нет");
