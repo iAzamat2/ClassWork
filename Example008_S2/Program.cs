9507bf01fe6931200fdaec759ae5c2988ba4870d
Azamat <isabirovazamati@gmail.com>
2022-07-10 19:19:17 +0500

    Add Example008 S2 Задача 12

---
+++ Example008_S2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+12. Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+*/
+
+void Zadacha12()
+{
+    Console.WriteLine("Введите первое число: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    int test=B % A;
+    Console.WriteLine((test) == 0 ? $"Число {B} кратно {A}" : $"Остаток от деления: {(test)}");
+}
+Zadacha12();
