6ee1057b262d9015d4d146a4edf8f54e159f9b74
Azamat <isabirovazamati@gmail.com>
2022-07-10 19:18:16 +0500

    Add Example007 S2 Задача 11

---
+++ Example007_S2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+*/
+
+void Zadacha11()
+{
+    Random rand = new Random();
+    int number = rand.Next(100, 1000);
+    Console.WriteLine(number);
+    int digit1 = number % 10;
+    //int digit2 = number / 10 % 10;
+    int digit3 = number / 100;
+    int a = (digit3 * 10) + digit1;
+    Console.WriteLine(a);
+}
+
+Zadacha11();
