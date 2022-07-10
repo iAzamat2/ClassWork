ae26c4f26034d20e7fc1e6e87c5c3b8a88c8f9ca
Azamat <isabirovazamati@gmail.com>
2022-07-10 19:14:41 +0500

    Add Example006 S2 Задача 9

---
+++ Example006_S2/Program.cs
@@ -0,0 +1,11 @@
+﻿void Zadacha9()
+{
+    Random rand = new Random();
+    int number = rand.Next(10, 100);
+    Console.WriteLine(number);
+    int digitOnes = number/10;
+    int digitTens = number%10;
+    Console.WriteLine((digitOnes>digitTens? $"Наибольшая цифра числа: {digitOnes}":$"Наибольшая цифра числа: {digitTens}"));
+}
+
+Zadacha9();
