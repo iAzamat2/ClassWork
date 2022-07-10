63d5ed9381ff71d31fa38ae1e5b2ea055bd73480
Azamat <isabirovazamati@gmail.com>
2022-07-10 19:20:16 +0500

    Add Example009 S2 Задача 14

---
+++ Example009_S2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+*/
+
+void Zadacha14()
+{
+    Console.WriteLine("Введите число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    bool test = (a % 7 == 0) & (a % 23 == 0);
+    if (test == true)
+    {
+        Console.WriteLine($"Число {a} кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} не кратно 7 и 23");
+    }
+}
+Zadacha14();
