81ab997c966e13dd7032a42184df8fe52be8323e
Azamat <isabirovazamati@gmail.com>
2022-08-13 19:55:10 +0500

    Добавлены задачи из 7 семинара

---
+++ Example022/Program.cs
@@ -0,0 +1,147 @@
+﻿using FunctionsOfArray;
+
+FunctionsOfArrayClass ar = new FunctionsOfArrayClass();
+
+/*
+Задача 46: Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+
+*/
+
+void Task46()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    ar.FillArray(array);
+    ar.PrintArray(array);
+}
+
+
+
+
+
+/*
+Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: A = i+j. 
+Выведите полученный массив на экран.
+
+*/
+
+void Task48()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    ar.PrintArray(array);
+}
+
+
+
+
+
+/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у 
+которых оба индекса чётные, и замените эти элементы на их 
+квадраты.
+
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Новый массив будет выглядеть 
+вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4
+
+*/
+
+void Task49()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    ar.FillArray(array, 1, 9);
+    ar.PrintArray(array);
+    Console.WriteLine();
+    SqrEven(array);
+    ar.PrintArray(array);
+
+    void SqrEven(int[,] array)
+    {
+        for (int i = 0; i < rows; i += 2)
+        {
+            for (int j = 0; j < columns; j += 2)
+            {
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    array[i, j] = (int)Math.Pow(array[i, j], 2);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+/*
+Задача 51: Задайте двумерный массив. Найдите сумму 
+элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+
+*/
+
+
+void Task51()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    ar.FillArray(array);
+    ar.PrintArray(array);
+    SumDiag(array);
+
+    void SumDiag(int[,] array)
+    {
+        int sum = 0;
+        int minindex = (rows < columns) ? rows : columns;
+        Console.Write($"Сумма элементов главной диагонали: ");
+        for (int i = 0; i < minindex; i++)
+        {
+            Console.Write(array[i, i]+"+");
+            sum += array[i, i];
+        }
+        Console.Write($"\b = {sum}\n");
+    }
+}
+
+
+//Task46();
+//Task48();
+//Task49();
+//Task51();
---
+++ Example022/functions.cs
@@ -0,0 +1,36 @@
+namespace FunctionsOfArray
+{
+    public class FunctionsOfArrayClass
+    {
+
+        public void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+        {
+            finishNumber++;
+            Random random = new Random();
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = random.Next(startNumber, finishNumber);
+                }
+            }
+        }
+
+        public void PrintArray(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
